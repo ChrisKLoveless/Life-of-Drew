3b5e044b2794fc989bd951817cab6c7de02c9102
Chris Loveless <chriskloveless@gmail.com>
2022-11-24 11:58:13 -0800

    update css styling and add font

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,24 +1,24 @@
 window.onload = function() {
   let body = document.querySelector("body")
   
-  let darkbutton = document.querySelector("button.btn.btn-dark");
+  let darkbutton = document.getElementById("dark-mode");
   darkbutton.onclick = function() {
   body.style.backgroundColor = "black";
   body.style.color = "white";
   };
 
-  let lightbutton = document.querySelector("button.btn.btn-light");
+  let lightbutton = document.getElementById("light-mode");
   lightbutton.onclick = function() {
     body.style.backgroundColor = "white";
     body.style.color = "black";
   };
 
-  let largebutton = document.querySelector("button.Large.Font");
+  let largebutton = document.getElementById("large-mode");
   largebutton.onclick = function() {
     body.style.fontSize = "24px";
   };
 
-  let tinybutton = document.querySelector("button.Tiny.Font");
+  let tinybutton = document.getElementById("tiny-mode");
   tinybutton.onclick = function() {
     body.style.fontSize = "10px";
   };
